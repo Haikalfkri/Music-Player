6a1ca5ee2fe6f52219ff1a27a9f2cb60f8be9738
Haikal Fikri <haikaljr1245@gmail.com>
2023-12-20 20:55:20 +0700

    setting model

--- musicPlayer/music_app/models.py
+++ musicPlayer/music_app/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="profile_images/")
+    
 class Song(models.Model):
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
@@ -15,7 +20,7 @@ class Song(models.Model):
 class Playlist(models.Model):
     title = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    songs = models.ManyToManyField(Song, on_delete=models.CASCADE)
+    songs = models.ManyToManyField(Song, blank=True)
     
     def __str__(self):
         return self.title
